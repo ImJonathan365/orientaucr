2b356da89da760333eb386f57b82e21570dfd405
Luis220922 <150977228+Luis220922@users.noreply.github.com>
2025-05-18 12:14:41 -0600

    controllerUser

--- src/main/java/cr/ac/ucr/orientaucr/orientaucr/controller/UserController.java
+++ src/main/java/cr/ac/ucr/orientaucr/orientaucr/controller/UserController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
+@Controller
  @RequestMapping("/api/user")
 
 public class UserController {
