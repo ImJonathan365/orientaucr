27d9c85d0c308c0abf7f9a23a535096e5b940f9a
Luis220922 <150977228+Luis220922@users.noreply.github.com>
2025-05-18 00:25:39 -0600

    Cambio

--- src/main/java/cr/ac/ucr/orientaucr/orientaucr/controller/UserController.java
+++ src/main/java/cr/ac/ucr/orientaucr/orientaucr/controller/UserController.java
@@ -1,4 +1,5 @@
 package cr.ac.ucr.orientaucr.orientaucr.controller;
+
 import cr.ac.ucr.orientaucr.orientaucr.domain.User;
 import cr.ac.ucr.orientaucr.orientaucr.service.UserService;
 import java.util.Collections;
@@ -10,40 +11,42 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
 @Controller
- @RequestMapping("User")
+@RequestMapping("User")
 public class UserController {
 
     @GetMapping("/listUser")
-   @ResponseBody
-    public Map getListUser(){
-    return Collections.singletonMap("data",UserService.getAllUsers());
-    }
-     @PostMapping("/add")
     @ResponseBody
-    public Map addUser(@RequestBody User user){
-       UserService.createUser(user);
-    return getListUser();
+    public Map getListUser() {
+        return Collections.singletonMap("data", UserService.getAllUsers());
     }
-    @PostMapping("/update")
-@ResponseBody
-public Map updateUser(@RequestBody User user) {
-    UserService.updateUser(user);
-    return getListUser();
-}
-@PostMapping("/delete")
-@ResponseBody
-public Map deleteUser(@RequestBody Map<String, String> body) {
-    String id = body.get("id");
-    UserService.deleteUser(id);
-    return getListUser();
-}
-@PostMapping("/find")
-@ResponseBody
-public User findUser(@RequestBody Map<String, String> body) {
-    String id = body.get("id");
-    return UserService.findById(id);
-}
 
+    @PostMapping("/add")
+    @ResponseBody
+    public Map addUser(@RequestBody User user) {
+        UserService.createUser(user);
+        return getListUser();
+    }
+
+    @PostMapping("/update")
+    @ResponseBody
+    public Map updateUser(@RequestBody User user) {
+        UserService.updateUser(user);
+        return getListUser();
+    }
+
+    @PostMapping("/delete")
+    @ResponseBody
+    public Map deleteUser(@RequestBody Map<String, String> body) {
+        String id = body.get("id");
+        UserService.deleteUser(id);
+        return getListUser();
+    }
+
+    @PostMapping("/find")
+    @ResponseBody
+    public User findUser(@RequestBody Map<String, String> body) {
+        String id = body.get("id");
+        return UserService.findById(id);
+    }
 }
